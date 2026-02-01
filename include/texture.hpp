d7d7985faa1aebfd2506a979fc39f4bce43c7520
Xelpi <ankith3690@gmail.com>
2026-02-01 21:00:18 +0530

    Added enumes for wrap and filter

--- include/texture.hpp
+++ include/texture.hpp
@@ -3,13 +3,32 @@
 #include <stb_image/stb_image.h>
 #include <glad/gl.h>
 #include <string>
+#include <optional>
+#include <glm/vec4.hpp>
+
+enum class TexFilter {
+    Nearest,
+    Linear,
+};
+
+enum class TexWrap {
+    Repeat,
+    MirroredRepeat,
+    ClampToEdge,
+    ClampToBorder,
+};
 
 class Texture {
     public:
 
     GLuint ID = 0;
 
-    Texture(const std::string& texPath);
+    Texture(
+        const std::string& path,
+        TexFilter filter,
+        TexWrap wrap,
+        std::optional<glm::vec4> borderColor
+    );
 
     ~Texture();
 
--- src/texture.cpp
+++ src/texture.cpp
@@ -1,6 +1,13 @@
 #include <texture.hpp>
 
-Texture::Texture(const std::string& texPath) {
+Texture::Texture(
+    const std::string& path,
+    TexFilter filter,
+    TexWrap wrap,
+    std::optional<glm::vec4> borderColor
+) {
+    glGenTextures(1, &ID);
+    glBindTexture(GL_TEXTURE_2D, ID);
 
 }
 
